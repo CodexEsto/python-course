5302823642978e53496eec54dab8ccd3d969c0ed
El khadir Safouane <elkhadirsafouane@gmail.com>
2025-02-01 15:29:07 +0100

    Updated files with new changes

--- basics/for loops/exercices/for_ex1.py
+++
@@ -1,5 +0,0 @@
-# ex1 :
-# TODO : Demandez à l'utilisateur deux nombres entre 1 et 100. 
-# TODO   Ensuite, comptez du plus petit au plus grand et affichez les résultats à l'écran.
-
-
--- basics/for loops/exercices/for_ex3.py
+++
@@ -1,2 +0,0 @@
-# ex3 :
-# TODO : Demandez à l'utilisateur un nombre entre 1 et 12, puis affichez sa table de multiplication.
